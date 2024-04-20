b3f79232187b600a8e0a299abddc615b7afce12e
Kavin <indranigk@gmail.com>
2024-04-20 09:17:16 +0530

    Commit-2

---
+++ JavaSwing/src/myShare/LoginAndSignUp.java
@@ -0,0 +1,21 @@
+package myShare;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Kavin
+ */
+public class LoginAndSignUp {
+    
+    public static void main(String[] args){
+        Login LoginFrame = new Login();
+        LoginFrame.setVisible(true);
+        LoginFrame.pack();
+        LoginFrame.setLocationRelativeTo(null);
+    }
+    
+}
